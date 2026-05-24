6e9491350ae04e9e8df7aa0b822f2e5ca79aebdd
aq-gauss <aquinones@itba.edu.ar>
2026-05-24 14:33:25 -0400

    Created main file with username validation

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+#define MIN_USERNAME 2
+#define MAX_USERNAME 10
+
+#define INVALID_USER 0
+#define VALID_USER 1
+
+int validate_username(const char*);
+
+int main(void){
+
+	char username[MAX_USERNAME+2]; //We make sure that nor the enter nor the terminating character get clipped
+	int valid_user;
+
+
+	printf("Hi there!\nPlease enter your username to start playing!\n");
+
+
+	do //We save and verify the username
+	{
+		if(fgets(username, MAX_USERNAME, stdin) != NULL){
+			username[strcspn(username, "\n")] = 0; //We replace the \n by a terminating character
+		}
+		valid_user = validate_username(username);
+		if (!valid_user){
+			printf("Invalid username, please try again\n");
+		}
+	}
+
+	while(!valid_user); //We will prompt the user again until the username becomes valid
+
+	printf("Welcome to Arkanoid in C, %s!\n", username);
+
+
+
+
+
+
+
+	return 0;
+}
+
+int validate_username(const char*username){
+	int length = strlen(username);
+	if (length<MIN_USERNAME||length>MAX_USERNAME){
+		return INVALID_USER; //If the username is longer or shorter it will be assumed invalid
+	}
+
+	for (int i=0; i<length;i++){
+		char letter = username[i];
+		if (!isalpha(letter) || isspace(letter)){
+			return INVALID_USER;
+		}
+	}
+	 //If the username passed the filter, we know is valid.
+	return VALID_USER;
+
+}
+
